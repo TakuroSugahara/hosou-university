466add9ded4ee499bfde4429019ef7572ecfef6a
TakuroSugahara <k5690033@gmail.com>
2021-04-21 09:40:44 +0900

    chapter-6, q-4

---
+++ algorizm-programing/chapter-6/q-4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char s0[] = "aaaaa";
+  char s1[] = "bbbbb";
+  char s2[] = "aaaaaaa";
+  int i;
+  printf("strnvmp(str1, str2)\n");
+  i = strncmp(s0, s0, 3);
+  printf("[%s] [%s] (%d)\n", s0, s0, i);
+
+  i = strncmp(s0, s1, 3);
+  printf("[%s] [%s] (%d)\n", s0, s1, i);
+
+  i = strncmp(s1, s0, 3);
+  printf("[%s] [%s] (%d)\n", s1, s0, i);
+
+  i = strncmp(s0, s2, 3);
+  printf("[%s] [%s] (%d)\n", s0, s2, i);
+
+  return 0;
+}
+
